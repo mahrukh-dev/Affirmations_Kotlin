871e9c434b69f2f5f233460e7d63c64493068b7d
MahRukh <mahrukhdev@gmail.com>
2023-08-03 15:35:31 +0500

    Affirmation data class, data source class, strings.xml updated

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val size: TextView = findViewById(R.id.size)
+        size.text = Datasource().loadAffirmations().size.toString()
+
     }
 }
