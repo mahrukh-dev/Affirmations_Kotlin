1e8c7c72e34e493031803323018269ad54e206bc
MahRukh <mahrukhdev@gmail.com>
2023-08-03 16:48:12 +0500

    adding images in recyclerView

---
+++ README.md
@@ -0,0 +1,14 @@
+# Affirmations App
+
+The Affirmations app is a simple Android application that displays positive affirmations in a RecyclerView. Affirmations are displayed one by one in a list format, and the user can scroll through the list to read different affirmations.
+
+## Features
+
+- Display a list of positive affirmations.
+- Use RecyclerView to efficiently display the list of affirmations.
+- Utilize an adapter class to bind the affirmation data to the RecyclerView.
+- Define a data class `Affirmation` to represent each affirmation item with a string resource ID as a parameter.
+- Create a `DataSource` class to load affirmations and provide them to the RecyclerView.
+
+
+
--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -13,6 +14,7 @@ class ItemAdapter(private val context: Context, private val list: List<Affirmati
 
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view){
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
 
@@ -29,6 +31,7 @@ class ItemAdapter(private val context: Context, private val list: List<Affirmati
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = list[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
 }
