581540624e9dcf767f8e45305de47803947fac91
MahRukh <mahrukhdev@gmail.com>
2023-08-03 16:16:20 +0500

    implementation of recycler view and its adapter

---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val list: List<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view){
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+
+    //invoked by layout manager
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun getItemCount() = list.size
+
+    //replace content of view
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = list[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+}
